5f21ec5fbec300adbf41dad72681c1fb358695aa
Richard Ky <93296008+richard-ky@users.noreply.github.com>
2024-01-08 22:34:40 -0800

    Add files via upload

--- E-I Plasticity FTSTS Desynchronization/open-loop.sh
+++ E-I Plasticity FTSTS Desynchronization/open-loop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-#SBATCH --job-name=desync
-#SBATCH --partition=gpu
-#SBATCH --output=desync.txt
+#SBATCH --job-name=open-loop
+#SBATCH --partition=compute
+#SBATCH --output=output.txt
 #
-#SBATCH --cpus-per-task=56
+#SBATCH --cpus-per-task=4
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=1000
 #SBATCH --mail-user=richard.ky@sjsu.edu
 #SBATCH --mail-type=BEGIN,END
 
 module load matlab
 
-matlab -nodisplay -nosplash -nodesktop -r "run('Data_Collection_Desynchronization.m')", exit
+matlab -nodisplay -nosplash -nodesktop -r "run('main_sim_stim.m')", exit
