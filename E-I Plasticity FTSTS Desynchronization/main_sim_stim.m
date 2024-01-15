b9b51b4c8678b49ad54db23a36ad202f75d966e9
Richard Ky <93296008+richard-ky@users.noreply.github.com>
2024-01-14 22:05:43 -0800

    Add files via upload

--- E-I Plasticity FTSTS Desynchronization/main_sim_stim.m
+++ E-I Plasticity FTSTS Desynchronization/main_sim_stim.m
@@ -84,6 +84,8 @@ for trial = 1:nJIE
     disp(['Data Collection # ',' JIE ',num2str(J_IE),'AmpE',num2str(VstimE),'AmpI',num2str(VstimI),'PW',num2str(T_stim)])
 
         [REf,timef,W_IEA] = long_sim(Ns,Nsf,duration,Stimoffduration,step,N_E,N_I,T_stim,x_neutral,VstimE,VstimI,dxlE,dxlI,vE0,vI0,S_EI0,S_IE0,S_II0,S_EE0,X_EI0,X_IE0,X_EE0,X_II0,Apost0,Apre0,W_IE0,W_EI0,W_II0,W_EE0,J_EI,J_EE,J_IE,J_II,S_key_EI,S_key_IE,S_key_EE,S_key_II,leftover_S_EI,leftover_S_IE,leftover_S_EE,leftover_S_II,spt_E0);
-
-    save(['Stim_data_analysis_',' JIE ',num2str(J_IE),'AmpE',num2str(VstimE),'AmpI',num2str(VstimI),'PW',num2str(T_stim), '.mat'],'REf','timef','W_IEA','-v7.3')
+    
+    folderName = strcat('E',num2str(E),'I',num2str(I),'PW',num2str(T))
+    mkdir(folderName)
+    save([folderName,'/Stim_data_analysis_',' JIE ',num2str(J_IE),'AmpE',num2str(VstimE),'AmpI',num2str(VstimI),'PW',num2str(T_stim), '.mat'],'REf','timef','W_IEA','-v7.3')
 end
