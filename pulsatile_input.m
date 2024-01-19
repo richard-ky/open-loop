07b8668bfc61f88f6f294e0ec3a20f896f0d49e4
Richard Ky <93296008+richard-ky@users.noreply.github.com>
2024-01-19 03:27:05 -0800

    Add files via upload

--- pulsatile_input.m
+++ pulsatile_input.m
@@ -1,10 +1,16 @@
 function [Ue, Ui] = pulsatile_input(multi,V_stimE,V_stimI,T_stim,x,duration,step,offduration)
+
+
+
+interval_time = 4*4*(x*T_stim+T_stim+multi*T_stim);
+duration = interval_time*1;
+
 Ue = zeros(1,duration/step);
 Ui = zeros(1,duration/step);
 duration1 = duration-offduration; 
 
 % deals with any machine error in calcuation
-epsilon = 0.001;
+epsilon = 0.01;
 
 % ue input
 counter = 0;
@@ -12,7 +18,7 @@ for i = 1:duration1/step
     counter = counter + step;
     
     % anodic phase
-    if counter >= step && counter < T_stim+step-epsilon
+    if counter >= 0 && counter < T_stim+step-epsilon
         Ue(i) = -V_stimE/multi;
     end
     
@@ -21,11 +27,15 @@ for i = 1:duration1/step
         Ue(i) = V_stimE;
     end
     
-    if counter >= (multi+1)*T_stim + step - epsilon && counter < (multi+1+x)*T_stim  + step - epsilon
+    if counter >= (multi+1)*T_stim + step && counter < (multi+1+x)*T_stim  + step 
         Ue(i) = 0;
     end
-    
-    if counter >= (multi+1+x)*T_stim -  epsilon
+
+    if counter >= (2+x)*T_stim  + step && counter < (2+x+multi-1)*T_stim
+        Ue(i) = 0;%-V_stim/multi;
+    end
+
+    if counter >= (2+multi-1+x)*T_stim -0.01
         counter = 0;
         Ue(i) = 0;
     end
@@ -40,7 +50,7 @@ for i = 1:duration1/step
     counter = counter + step;
     
     % cathodic phase
-    if counter >= step && counter < T_stim+step-epsilon
+    if counter > 0 && counter < T_stim+step-epsilon
         Ui(i) = V_stimI;
     end
     
@@ -50,11 +60,11 @@ for i = 1:duration1/step
     end
     
     % neutral phase
-    if counter >= (multi+1)*T_stim + step - epsilon && counter < (multi+1+x)*T_stim  + step - epsilon
+    if counter >= (multi+1)*T_stim + step  && counter < (multi+1+x)*T_stim  
         Ui(i) = 0;
     end
     
-    if counter >= (multi+1+x)*T_stim -  epsilon
+    if counter >= (multi+1+x)*T_stim -  0.01
        counter = 0;
        Ui(i) = 0;
     end
